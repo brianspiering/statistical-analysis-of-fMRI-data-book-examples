5013d8a7b77df12ca2b582c5d9754b61cbd4e58e
brianspiering <bspiering@gmail.com>
2013-08-13 20:36:20 -0700

    started work on chapter 4

---
+++ box3_3.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+time = np.arange(0,50,.01) # Define the time axis.
+# TODO: define # bias function #1
---
+++ box4_2.py
@@ -0,0 +1,10 @@
+import numpy as np
+import random
+
+# Define x.
+x                 = np.ones(shape=(20,20))*5 # define entire area
+x[5:14,5:14]     = x[5:14,5:14] + 7    # "raise" section
+x[5:8,5:8]         = x[5:8,5:8] + 5       # "raise" section
+x[9:11,9:11]     = x[9:11,9:11] + 35  # "raise" section
+x[12:14,12:14] = x[12:14,12:14]+27 # "raise" section
+n = random.normalvariate(0,1.5) # TODO: make it 20x20 array
