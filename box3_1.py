a1d5136522d5b8d9e68c161878b40564f53b2ec7
brianspiering <bspiering@gmail.com>
2013-08-23 12:19:33 -0700

    exactly mirror MATLAB code

--- box3_1.py
+++ box3_1.py
@@ -10,7 +10,7 @@ n = 4 		#
 lamda = 2	#
 
 # Define the time axis.
-time = np.arange(0,30,.01)  # from 0 to 30 sec in 1 msec increments
+time = np.arange(0,30.01,.01)  # from 0 to 30 sec in 1 msec increments
 
 # Define the hrf.
 hrf = ((time-t0)**(n-1))**np.exp(-(time-t0)/lamda)/((lamda**n)*
