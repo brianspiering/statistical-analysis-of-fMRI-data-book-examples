02b836fa81a5021d9f8600fc0f3baba23d4ab483
Brian Spiering <bspiering@gmail.com>
2013-11-21 09:45:05 -0800

    Automated test for all scalar values
    Next up is testing vectors

--- test_box3_1.py
+++ test_box3_1.py
@@ -1,8 +1,5 @@
 # Test Box 3_1.py
 
-# TODO:  Learn TDD
-# Refactor to generalize to all files
-
 import unittest, os
 import scipy.io
 
@@ -10,10 +7,10 @@ def load_matlab_data_file(directory, filename):
     return scipy.io.loadmat(directory + filename) # load MATLAB file into dict
 
 class TestBoxAll(unittest.TestCase):
-  
-    # Test T0 value
-    def test_T0(self):
-        current_file_name = 'box3_1'
+      
+    # Test all dict keys 
+    def test_all_keys(self):
+        current_file_name = 'box3_1' # TODO: Generalize to all files
         
         # Load MATLAB data, aka the ground truth
         directory = os.getcwd()  + '/matlab_files/'
@@ -21,11 +18,15 @@ class TestBoxAll(unittest.TestCase):
         
         # Load Python data
         python_data = __import__(current_file_name)
-        # TODO: turn off plot upon import via __main__
-        
-        self.assertEqual(matlab_data['T0'], python_data.T0)
-       
-    # TODO: Test all dict keys
+        # TODO: turn off plot upon import
+    
+        for k in matlab_data.keys():
+            if not k.startswith('__'): # ignore class attributes
+                if matlab_data[k].shape[1] == 1: # ignore nonscalar-valued arrays
+                    # TODO: add test for vector values
+                    current_python_variable_value = eval("python_data." + k)
+                    self.assertEqual(matlab_data[k], 
+                                     current_python_variable_value)
 
 if __name__== "__main__":
     unittest.main()
