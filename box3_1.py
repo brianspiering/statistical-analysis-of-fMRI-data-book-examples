ecc58917e8d2b2ca16a52efa803ded0139bc3317
Brian Spiering <bspiering@gmail.com>
2013-11-27 09:29:27 -0800

    Fiddling

--- box3_1.py
+++ box3_1.py
@@ -9,11 +9,11 @@ n = 4 		# a hrf shape parameter
 lamda = 2	# a hrf shape parameter
 
 # Define the time axis.
-time = arange(0,30.01,.01)  # from 0 to 30 sec in 1 msec increments
+time = arange(0, 30.01, .01)  # from 0 to 30 sec in 1 msec increments
 
 # Define the hrf.
 from hrf import hrf
 
 # Plot the hrf.
-plot(time,hrf(T0, time, n, lamda))
+plot(time, hrf(T0, time, n, lamda))
 show()
--- scratchpad.py
+++ scratchpad.py
@@ -1,3 +1,21 @@
-# Debugging test_hrf
+## Debugging test_hrf
 
-t = TestHRFValues
+time = arange(0,10,1)
+n = 4
+
+# ((time-t0)**(n-1))**exp(-(time-t0)/lamda)/((lamda**n)*factorial(n-1))
+
+
+## Define parameters of hemodynamic response function (hrf).
+#T0 = 0 	# Lag between stimulus presentation and inital BOLD rise
+#n = 4 		# a hrf shape parameter
+#lamda = 2	# a hrf shape parameter
+# 
+# 
+## Define the time axis.
+#from numpy import arange
+#time = arange(0, 30.01, .01)  # from 0 to 30 sec in 1 msec increments 
+# 
+#from hrf import hrf
+#
+#hrf(T0, time, n, lamda)
